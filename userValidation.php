956445c5c7236bf0be8d90c825c04e37d55b6a2c
indranil-innoraft <indranil.roy@innoraft.com>
2023-02-15 10:22:42 +0530

    FT2023-55:adding Proper comment.

--- userValidation.php
+++ userValidation.php
@@ -1,21 +1,33 @@
 <?php
-  /** 
-  * @param string $userEmail
-  * @param string $userPassword
-  * @method validation()
-  *  to check user  have enter a correct username and password.
-  **/
 
-  session_start();
+   session_start();
+
+  /** 
+  * @param string $userEmail.
+  * @param string $userPassword.
+  **/
 
   $userEmail = $_POST['email'];
   $userPassword = $_POST['password'];
   
+  /**
+   * validation function
+   *
+   * @param string $userEmail
+   * @param string $userPassword
+   * 
+   * @return void
+   *  if user input is correct then it redirect to Assignment task page.
+   *  if user input is invalid then it reditect to the login page.
+   * 
+   */
+  
   function validation($userEmail,$userPassword){
-    if ($userEmail == 'indranil.roy@innoraft.com' && $userPassword == 'innoraft') {
-      session_start();
+    if ($userEmail === 'indranil.roy@innoraft.com' && $userPassword === 'innoraft') {
       $_SESSION['login'] = true;
+      // set user name 
       $_SESSION['name']="Indranil Roy";
+      // set user email address.
       $_SESSION['email']="indranil.roy@innoraft.com";
       header('Location: form/login.php');
     }
@@ -24,7 +36,6 @@
       header('Location: index.php');
     }
   }
-  
-  validation($userEmail,$userPassword);
 
-  ?>
+  //call the validation function.
+  validation($userEmail,$userPassword);
