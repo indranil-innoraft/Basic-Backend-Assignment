9681f6f228af778241b0eaab75294c1f752bff8a
indranil-innoraft <indranil.roy@innoraft.com>
2023-02-16 14:03:53 +0530

    FT2023-55:adding checkUploadedFile method to check user uploaded a right image or not.

--- validation.php
+++ validation.php
@@ -3,27 +3,51 @@
 session_start();
 
 
-
-
 class Validation
 {
+  /**
+   * @method boolean checkUserName()
+   *   check user enter a valid name or not.
+   * @method boolean checkUpLoadedFile().
+   *   check user upload a valid image or not.
+   */
 
   public function checkUserName($firstName, $lastName)
   {
     if (empty($firstName) || empty($lastName)) {
-      $_SESSION['formErrorMsg'] = "field should not be empty.";
+      $_SESSION['formErrorMsg'] = "Name should not be empty.";
       return false;
-    } 
-    else if (is_numeric($firstName) || is_numeric($lastName)) {
-      $_SESSION['formErrorMsg'] = "field should be alphabet.";
+    } else if (preg_match('/[0-9]/', $firstName) || preg_match('/[0-9]/', $lastName)) {
+      $_SESSION['formErrorMsg'] = "Name should be contain alphabet.";
       return false;
-    } 
-    else if (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $firstName) || preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $lastName)) {
-      $_SESSION['formErrorMsg'] = "field should not be special character.";
+    
+    } else if (preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $firstName) || preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $lastName)) {
+      $_SESSION['formErrorMsg'] = "Name should not contain special character.";
       return false;
+    } else {
+      return true;
+    }
+  }
+
+  function checkUploadedFile($fileName, $tempName, $filePath, $type, $size)
+  {
+    if (isset($fileName)) {
+      if ($type != "image/png"  && $type != "image/jpeg" && $type != "image/jpg") {
+        $_SESSION['formErrorMsg'] = "please upload a image(jpeg,png or png).";
+        return false;
+      }
+      if ($size > 6291456) {
+        $_SESSION['formErrorMsg'] = "please upload a image less then 6MB";
+        return false;
+      }
+      $path = "upload_image/" . $fileName;
+      $_SESSION['uploadedImage'] = $path;
+      move_uploaded_file($tempName, $path);
+      return true;
     } 
     else {
-      return true;
+      $_SESSION['formErrorMsg'] = "please upload a image";
+      return false;
     }
   }
 }
